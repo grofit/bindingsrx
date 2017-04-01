74b857f3008ed501c582a2388b1bacbbd9a3d099
LP <grofit1@yahoo.com>
2017-04-01 20:06:15 +0100

    Fixed issue with SliderValue binding
    The slider value binding was setting minvalue

--- src/Assets/BindingsRx/Bindings/SliderExtensions.cs
+++ src/Assets/BindingsRx/Bindings/SliderExtensions.cs
@@ -8,10 +8,10 @@ namespace BindingsRx.Bindings
     public static class SliderExtensions
     {
         public static IDisposable BindValueTo(this Slider input, IReactiveProperty<float> property, BindingTypes bindingType = BindingTypes.Default, params IFilter<float>[] filters)
-        { return GenericBindings.Bind(() => input.value, x => input.maxValue = x, property, bindingType, filters).AddTo(input); }
+        { return GenericBindings.Bind(() => input.value, x => input.value = x, property, bindingType, filters).AddTo(input); }
 
         public static IDisposable BindValueTo(this Slider input, Func<float> getter, Action<float> setter, BindingTypes bindingType = BindingTypes.Default, params IFilter<float>[] filters)
-        { return GenericBindings.Bind(() => input.value, x => input.maxValue = x, getter, setter, bindingType, filters).AddTo(input); }
+        { return GenericBindings.Bind(() => input.value, x => input.value = x, getter, setter, bindingType, filters).AddTo(input); }
 
         public static IDisposable BindMaxValueTo(this Slider input, IReactiveProperty<float> property, BindingTypes bindingType = BindingTypes.Default, params IFilter<float>[] filters)
         { return GenericBindings.Bind(() => input.maxValue, x => input.maxValue = x, property, bindingType, filters).AddTo(input); }
